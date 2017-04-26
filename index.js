21c4b0344939f3c833c2db6134086a98bc81cf80
Kevin Altman <kevin@itsthatguy.com>
2017-04-26 16:23:42 -0400

    base script

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require('fs-extra');
+const download = require('download-git-repo');
+
+const FILES_TO_REMOVE = [
+  'yarn.lock',
+];
+
+download('itsthatguy/base-react-app', '.', (error) => {
+  if (error) return console.log('Error download repository.', error);
+
+  FILES_TO_REMOVE.map((filename) => {
+    fs.removeSync(filename);
+  });
+
+  console.log('Success!');
+});
